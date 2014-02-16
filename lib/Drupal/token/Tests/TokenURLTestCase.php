8990130db3dc3a740b5ecebbfe8e8f1a4530adf2
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-02-16 14:41:26 +1300

    Issue #1962358 by grom358: Fixed TokenURLTestCase

--- lib/Drupal/token/Tests/TokenURLTestCase.php
+++ lib/Drupal/token/Tests/TokenURLTestCase.php
@@ -10,6 +10,8 @@ namespace Drupal\token\Tests;
  * Tests url tokens.
  */
 class TokenURLTestCase extends TokenTestBase {
+  protected static $modules = array('path', 'token', 'token_test', 'node');
+
   public static function getInfo() {
     return array(
       'name' => 'URL token tests',
@@ -18,30 +20,31 @@ class TokenURLTestCase extends TokenTestBase {
     );
   }
 
-  public function setUp($modules = array()) {
-    parent::setUp($modules);
+  public function setUp() {
+    parent::setUp();
     $this->saveAlias('node/1', 'first-node');
   }
 
   function testURLTokens() {
+    $host = \Drupal::request()->getHttpHost();
     $tokens = array(
-      'absolute' => 'http://example.com/first-node',
+      'absolute' => "http://{$host}/first-node",
       'relative' => base_path() . 'first-node',
-      'path' => 'first-node',
-      'brief' => 'example.com/first-node',
+      'path' => '/first-node',
+      'brief' => "{$host}/first-node",
       'args:value:0' => 'first-node',
       'args:value:1' => NULL,
       'args:value:N' => NULL,
-      'unaliased' => 'http://example.com/node/1',
+      'unaliased' => "http://{$host}/node/1",
       'unaliased:relative' => base_path() . 'node/1',
-      'unaliased:path' => 'node/1',
-      'unaliased:brief' => 'example.com/node/1',
+      'unaliased:path' => '/node/1',
+      'unaliased:brief' => "{$host}/node/1",
       'unaliased:args:value:0' => 'node',
       'unaliased:args:value:1' => '1',
       'unaliased:args:value:2' => NULL,
       // Deprecated tokens.
-      'alias' => 'first-node',
+      'alias' => '/first-node',
     );
-    $this->assertTokens('url', array('path' => 'node/1', 'options' => array('base_url' => 'http://example.com')), $tokens);
+    $this->assertTokens('url', array('route_name' => 'node.view', 'route_parameters' => array('node' => 1)), $tokens);
   }
 }
