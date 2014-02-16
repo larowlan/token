1200452b86010ce9bb1dc4f8157fd0bba97f0301
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-02-16 13:10:26 +1300

    Issue #1962358 by grom358: Fixed TokenFileTestCase

--- lib/Drupal/token/Tests/TokenFileTestCase.php
+++ lib/Drupal/token/Tests/TokenFileTestCase.php
@@ -10,6 +10,8 @@ namespace Drupal\token\Tests;
  * Tests file tokens.
  */
 class TokenFileTestCase extends TokenTestBase {
+  protected static $modules = array('path', 'token', 'token_test', 'file');
+
   public static function getInfo() {
     return array(
       'name' => 'File token tests',
