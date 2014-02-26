c1c84773ca0b85b31b95e9f5f002f62a9377a328
Christian Lopez Espinola <penyaskito@computer.org>
2014-02-26 01:31:55 +0100

    Issue #1962358 by penyaskito: Convert token/flush-cache to a controller.

---
+++ lib/Drupal/token/Controller/TokenCacheController.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\token\Controller\TokenCacheController.
+ */
+
+namespace Drupal\token\Controller;
+
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Clears cache for tokens.
+ */
+class TokenCacheController implements ContainerInjectionInterface{
+
+  /**
+   * @var \Drupal\Core\Access\CsrfTokenGenerator
+   */
+  private $csrfToken;
+
+  /**
+   * Constructs a new TokenAutocompleteController.
+   */
+  public function __construct($csrf_token) {
+    $this->csrfToken = $csrf_token;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('csrf_token')
+    );
+  }
+
+  public function flush(Request $request) {
+    if (!$request->query->has('token') || ! $this->csrfToken->validate($request->query->get('token'), current_path())) {
+      return MENU_NOT_FOUND;
+    }
+
+    token_clear_cache();
+    drupal_set_message(t('Token registry caches cleared.'));
+    return $this->redirect($request->attributes->get(RouteObjectInterface::ROUTE_NAME), $request->attributes->get('_raw_variables', array()));
+  }
+
+}
