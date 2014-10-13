d6177bc5603de14449d8f791d51494da3f4fe12a
Kim Pepper <kim@pepper.id.au>
2014-10-14 08:43:59 +1100

    Fix token menu tests

--- src/Tests/TokenMenuTestCase.php
+++ src/Tests/TokenMenuTestCase.php
@@ -5,6 +5,8 @@
  * Contains \Drupal\token\Tests\TokenMenuTestCase.
  */
 namespace Drupal\token\Tests;
+use Drupal\Core\Url;
+use Drupal\system\Tests\Menu\MenuRouterTest;
 
 /**
  * Tests menu tokens.
@@ -12,51 +14,61 @@ namespace Drupal\token\Tests;
  * @group token
  */
 class TokenMenuTestCase extends TokenTestBase {
-  protected static $modules = array('path', 'token', 'token_test', 'menu_ui', 'node');
+  protected static $modules = array('path', 'token', 'token_test', 'menu_ui', 'menu_test', 'node');
 
   function testMenuTokens() {
-    // Add a menu.
-    $menu = entity_create('menu', array(
-      'id' => 'main-menu',
-      'label' => 'Main menu',
-      'description' => 'The <em>Main</em> menu is used on many sites to show the major sections of the site, often in a top navigation bar.',
-    ));
-    $menu->save();
-    // Add a root link.
-    $root_link = entity_create('menu_link_content', array(
-      'url' => 'admin',
-      'title' => 'Administration',
-      'menu_name' => 'main-menu',
-      'bundle' => 'menu_link_content',
-    ));
-    $root_link->save();
 
-    // Add another link with the root link as the parent
-    $parent_link = entity_create('menu_link_content', array(
-      'url' => 'admin/config',
-      'title' => 'Configuration',
-      'menu_name' => 'main-menu',
-      'parent' => 'menu_link_content:' . $root_link->uuid(),
-      'bundle' => 'menu_link_content',
-    ));
-    $parent_link->save();
+    /** @var \Drupal\Core\Menu\MenuLinkManagerInterface $menu_link_manager */
+    $menu_link_manager = \Drupal::service('plugin.manager.menu.link');
+    $menu_links = $menu_link_manager->loadLinksByRoute('token_test.root');
+    /** @var \Drupal\Core\Menu\MenuLinkInterface $root_link */
+    $root_link = $menu_links['token_test.root'];
+    $menu_links = $menu_link_manager->loadLinksByRoute('token_test.parent');
+    /** @var \Drupal\Core\Menu\MenuLinkInterface $parent_link */
+    $parent_link = $menu_links['token_test.parent'];
+
+//    // Add a menu.
+//    $menu = entity_create('menu', array(
+//      'id' => 'main-menu',
+//      'label' => 'Main menu',
+//      'description' => 'The <em>Main</em> menu is used on many sites to show the major sections of the site, often in a top navigation bar.',
+//    ));
+//    $menu->save();
+//    // Add a root link.
+//    $root_link = entity_create('menu_link_content', array(
+//      'url' => 'admin',
+//      'title' => 'Administration',
+//      'menu_name' => 'main-menu',
+//      'bundle' => 'menu_link_content',
+//    ));
+//    $root_link->save();
+//
+//    // Add another link with the root link as the parent.
+//    $parent_link = entity_create('menu_link_content', array(
+//      'url' => 'admin/config',
+//      'title' => 'Configuration',
+//      'menu_name' => 'main-menu',
+//      'parent' => 'menu_link_content:' . $root_link->uuid(),
+//      'bundle' => 'menu_link_content',
+//    ));
+//    $parent_link->save();
 
     // Test menu link tokens.
     $tokens = array(
-      'mlid' => $parent_link->id(),
+      'mlid' => $parent_link->getPluginId(),
       'title' => 'Configuration',
       'menu' => 'Main menu',
       'menu:name' => 'Main menu',
       'menu:machine-name' => 'main-menu',
       'menu:description' => 'The <em>Main</em> menu is used on many sites to show the major sections of the site, often in a top navigation bar.',
       'menu:menu-link-count' => 2,
-      'menu:edit-url' => url("admin/structure/menu/manage/main-menu", array('absolute' => TRUE)),
-      'url' => url('admin/config', array('absolute' => TRUE)),
-      'url:absolute' => url('admin/config', array('absolute' => TRUE)),
-      'url:relative' => url('admin/config', array('absolute' => FALSE)),
+      'menu:edit-url' => Url::fromUri("admin/structure/menu/manage/main-menu", array('absolute' => TRUE)),
+      'url' => Url::fromUri('admin/config', array('absolute' => TRUE)),
+      'url:absolute' => Url::fromUri('admin/config', array('absolute' => TRUE)),
+      'url:relative' => Url::fromUri('admin/config', array('absolute' => FALSE)),
       'url:path' => 'admin/config',
       'url:alias' => 'admin/config',
-      'edit-url' => url("admin/structure/menu/item/{$parent_link->id()}/edit", array('absolute' => TRUE)),
+      'edit-url' => Url::fromUri("admin/structure/menu/item/{$parent_link->id()}/edit", array('absolute' => TRUE)),
       'parent' => 'Administration',
       'parent:mlid' => $root_link->id(),
       'parent:title' => 'Administration',
@@ -78,7 +90,7 @@ class TokenMenuTestCase extends TokenTestBase {
     // Allow main menu for this node type.
     \Drupal::config('menu.entity.node.' . $node->getType())->set('available_menus', array('main-menu'))->save();
 
-    // Add a node menu link
+    // Add a node menu link.
     $node_link = entity_create('menu_link_content', array(
       'link_path' => 'node/' . $node->id(),
       'title' => 'Node link',
@@ -93,9 +105,9 @@ class TokenMenuTestCase extends TokenTestBase {
       'menu-link:mlid' => $node_link->id(),
       'menu-link:title' => 'Node link',
       'menu-link:menu' => 'Main menu',
-      'menu-link:url' => url('node/' . $node->id(), array('absolute' => TRUE)),
+      'menu-link:url' => Url::fromUri('node/' . $node->id(), array('absolute' => TRUE)),
       'menu-link:url:path' => 'node/' . $node->id(),
-      'menu-link:edit-url' => url("admin/structure/menu/item/{$node_link->id()}/edit", array('absolute' => TRUE)),
+      'menu-link:edit-url' => Url::fromUri("admin/structure/menu/item/{$node_link->id()}/edit", array('absolute' => TRUE)),
       'menu-link:parent' => 'Configuration',
       'menu-link:parent:mlid' => $parent_link->id(),
       'menu-link:parents' => 'Administration, Configuration',
---
+++ tests/token_test/src/Controller/TokenTestController.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\token_test\Controller\TokenTestController
+ */
+
+namespace Drupal\token_test\Controller;
+
+/**
+ * Token test controller.
+ */
+class TokenTestController {
+
+  /**
+   * Test controller method.
+   *
+   * @return string
+   *   The response.
+   */
+  public function test() {
+    return "Icecream!";
+  }
+}
